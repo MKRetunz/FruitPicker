a2242753d2a072e4b6df57b2f0aa3362ba5bc62d
MKRetunz <Martijnklok@hotmail.nl>
2016-11-15 10:23:04 +0100

    Added fruit spawning script to scene

--- FruitPicker/Assets/Scripts/FruitHandler.cs
+++ FruitPicker/Assets/Scripts/FruitHandler.cs
@@ -1,15 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class FruitHandler : MonoBehaviour {
+public class FruitHandler : MonoBehaviour
+{
+    public Transform Fruit;
+    float spawnTimer;
+    float spawnTime;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    // Use this for initialization
+    void Start()
+    {
+        spawnTime = 10f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        spawnTimer += Time.deltaTime;
+
+        if (spawnTimer >= spawnTime)
+        {
+            Instantiate(Fruit, new Vector3(transform.position.x, transform.position.y + 1, transform.position.z), Quaternion.identity);
+            spawnTimer = 0f;
+        }
+    }
 }
