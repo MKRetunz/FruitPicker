6ee2a9c5fb63d78c0b94b8045a51809a1f27e0f3
MKRetunz <Martijnklok@hotmail.nl>
2016-11-15 12:30:11 +0100

    Added Collision detection and shifted cause for Fruit spawning

--- FruitPicker/Assets/Scripts/FruitHandler.cs
+++ FruitPicker/Assets/Scripts/FruitHandler.cs
@@ -4,22 +4,32 @@ using System.Collections;
 public class FruitHandler : MonoBehaviour
 {
     public Transform Fruit;
-    private GameObject[] fruitList;
     float spawnTimer;
     float spawnTime;
     bool activeFruit= false;
 
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "Fruit(Clone)")
+        {
+            activeFruit = true;
+        }
+        else
+        {
+            activeFruit = false;
+        }
+    }
+
     // Use this for initialization
     void Start()
     {
-        fruitList = new GameObject[1] { Instantiate(Fruit, new Vector3(transform.position.x, transform.position.y + 1, transform.position.z), Quaternion.identity) as GameObject };
         spawnTime = 1f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (fruitList == null)
+        if (activeFruit == false)
         {
             spawnTimer += Time.deltaTime;
             Debug.Log("Heya");
@@ -27,7 +37,7 @@ public class FruitHandler : MonoBehaviour
 
         if (spawnTimer >= spawnTime)
         {
-            fruitList[0] = Instantiate(Fruit, new Vector3(transform.position.x, transform.position.y + 1, transform.position.z), Quaternion.identity) as GameObject;
+            Instantiate(Fruit, new Vector3(transform.position.x, transform.position.y + 1, transform.position.z), Quaternion.identity);
             
             spawnTimer = 0f;
         }
