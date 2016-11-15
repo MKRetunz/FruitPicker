9c563e5a4a7182062da80e6eb8d4d3419284983e
MKRetunz <Martijnklok@hotmail.nl>
2016-11-15 11:36:09 +0100

    Added collision script

---
+++ FruitPicker/Assets/Scripts/BasketHandler.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasketHandler : MonoBehaviour {
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "Fruit(Clone)")
+        {
+            Destroy(col.gameObject);
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
